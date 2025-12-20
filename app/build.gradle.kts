b411019ab9f670fb416893cad76953dcb019a6fe
Juliandev02 <julian@strawberryfoundations.org>
2025-12-20 21:19:49 +0100

    chore: update version name to 2.0.0-alpha2 and change 'Training' to 'Workout' in strings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 33
         targetSdk = 36
         versionCode = 1
-        versionName = "2.0.0-alpha1"
+        versionName = "2.0.0-alpha2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
