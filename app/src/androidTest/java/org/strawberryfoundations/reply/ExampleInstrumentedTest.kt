7842ef4d1b1eb35476be127dcb2985f0ad743163
Juliandev02 <julian@strawberryfoundations.org>
2025-12-24 14:55:46 +0100

    fix: correct package name in instrumented test

--- app/src/androidTest/java/org/strawberryfoundations/reply/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/org/strawberryfoundations/reply/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("org.strawberryfoundations.replicity", appContext.packageName)
+        assertEquals("org.strawberryfoundations.reply", appContext.packageName)
     }
 }
